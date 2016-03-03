c1e66e2ddc30d8de373eebfaca4b0159c54b6b5d
Chris Townsend <ctownsen357@gmail.com>
2016-03-03 07:43:39 -0500

    2nd excersite completed.

--- R-class/pollutantmean.R
+++ R-class/pollutantmean.R
@@ -1,3 +1,22 @@
+#complete returns the number of observations within the file ids 
+#contained in the specified directory that are complete (not NA)
+#trying to it a little more interesting by keeping everything as
+#vectorized as possible
+complete <- function(directory, id = 1:332){
+    padded_ids <- paste(sprintf("%03d", id), ".csv", sep = "")
+    padded_ids <- paste("/", padded_ids, sep = "")
+    padded_ids <- paste(directory, padded_ids, sep = "")
+    
+    file_lengths <- do.call(rbind, lapply(lapply(lapply(padded_ids, read.csv), complete.cases),sum))
+    
+    x_name <- "id"
+    y_name <- "nobs"
+    
+    df <- data.frame(id, file_lengths)
+    names(df) <- c(x_name, y_name)
+    df
+}
+
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     # "Date","sulfate","nitrate","ID"
     ## 'directory' is a character vector of length 1 indicating
@@ -28,3 +47,4 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     format.default(mean_val, digits = 4)
     
 }
+
--- R-class/readme.md
+++ R-class/readme.md
@@ -1,1 +1,2 @@
-+Code for excercises in the R course of the Data Science series of classes
+# Code for excercises in the R course of the Data Science series of classes
+Keeping this as a reference for how I've done various things in the class
